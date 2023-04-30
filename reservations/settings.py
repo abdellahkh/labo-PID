d91b26d733293930ef9af269ca1dcb17d1e0d319
Zakariya <z.jadimoussa@hotmail.fr>
2023-05-01 00:20:34 +0200

    After framework configuration

--- reservations/settings.py
+++ reservations/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'reservations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
