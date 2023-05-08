d82c424a779b05e0117570a2bb06b589850f8a97
abdellahkh <abdellah.khleeh@gmail.com>
2023-05-08 13:40:43 +0200

    me suis perdu

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=70)
+    lastName = models.CharField(max_length=70)
+    address = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
