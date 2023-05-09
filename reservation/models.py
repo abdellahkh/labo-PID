95af00528cbf2a68c5f5ef57a860ecb0c5b7421b
abdellahkh <abdellah.khleeh@gmail.com>
2023-05-09 18:28:08 +0200

    ajout carousel, travail effectue sur home page, debut affichage spectacle

--- reservation/models.py
+++ reservation/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Show(models.Model):
     slug = models.CharField(max_length=60)
     title = models.CharField(max_length=60)
-    description = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
     poster_url = models.CharField(max_length=255)
     locality_id = models.IntegerField()
     bookable = models.BooleanField()
     price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
