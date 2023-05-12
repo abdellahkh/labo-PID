32ec3e460a3c5c6f1b79352d74b4eae91d69611b
abdellahkh <abdellah.khleeh@gmail.com>
2023-05-12 13:14:15 +0200

    find import and exoprt data

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Show
+
 from .models import Location
 from .models import Locality
 from .models import Artist
@@ -15,11 +17,19 @@ from .models import RoleUser
 
 # Register your models here.
 @admin.register(Show)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'slug', 'title', 'description', 'poster_url', 'location_id', 'bookable', 'price', 'created_at')
 
-admin.site.register(Location)
-admin.site.register(Locality)
+@admin.register(Location)
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('id', 'slug', 'designation')
+
+
+@admin.register(Locality)
+class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('id', 'postal_code', 'locality')
+
+
 admin.site.register(Artist)
 admin.site.register(Type)
 admin.site.register(ArtisteType)
